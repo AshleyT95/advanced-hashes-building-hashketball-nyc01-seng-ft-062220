271d5a14a808c602b00336513dc1c7996c7733e7
Ashley Thompson <ashleythompsonbca@gmail.com>
2020-06-07 21:04:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,14 +2,15 @@
 def game_hash
   hashketball= {
     :home => {
-      :team_name => ()
-      color => []
-      :players => [
+      :team_name => () }
+      {:color => []}
+      {:players => [
         {:player_name => ,:number => , :shoe => , :points => , :rebounds => ,
         :assists => , :steals => , :blocks => , :slam_dunks =>
 
+      ]
       }
-      ] ,
+      } ,
   :away => {
     :team_name => ()
     :color => []
